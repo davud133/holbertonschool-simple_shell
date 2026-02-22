727bcb9fd57312de777bdc08c5fbbf903feaa0ef
davud <davudgeroqiq@gmail.com>
2026-02-22 22:04:08 +0300

    new commit

--- shell.c
+++ shell.c
@@ -1,163 +1,124 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-
-extern char **environ; /* needed for execve() */
-
+#include "main.h"
 /**
- * split_line - split input line into arguments
- * @line: input line
- * Return: NULL-terminated array of strings
+ * execute_command - func for executing command
+ * @args: arguments to path
+ * @path: path
  */
-char **split_line(char *line)
+void execute_command(char **args, char *path)
 {
-    int bufsize = 64, pos = 0;
-    char **tokens, *token;
+	pid_t pid;
+	int status;
 
-    tokens = malloc(sizeof(char *) * bufsize);
-    if (!tokens)
-    {
-        fprintf(stderr, "Allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    token = strtok(line, " \t\r\n");
-    while (token != NULL)
-    {
-        tokens[pos++] = token;
-
-        if (pos >= bufsize)
-        {
-            bufsize += 64;
-            tokens = realloc(tokens, sizeof(char *) * bufsize);
-            if (!tokens)
-            {
-                fprintf(stderr, "Allocation error\n");
-                exit(EXIT_FAILURE);
-            }
-        }
-        token = strtok(NULL, " \t\r\n");
-    }
-    tokens[pos] = NULL;
-    return tokens;
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork failed");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		if (execve(path, args, environ) == -1)
+		{
+			free(path);
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		if (wait(&status) == -1)
+		{
+			free(path);
+			perror("wait failed");
+			exit(EXIT_FAILURE);
+		}
+	}
 }
-
 /**
- * command_exists - check if command exists (absolute or relative)
- * @cmd: command name
- * Return: full path or NULL
- */
-char *command_exists(char *cmd)
+ * parse_command - func for parsing command
+ * @u_command: command to be parsed
+ * @args: arguments to command
+*/
+void parse_command(char *u_command, char **args)
 {
-    char *path_env, *path_dup, *dir, *fullpath;
-    char *path_sep = ":";
-    int len;
+	char *command = strtok(u_command, " \t");
+	int i = 0;
 
-    if (strchr(cmd, '/') != NULL)
-    {
-        if (access(cmd, X_OK) == 0)
-            return cmd;
-        return NULL;
-    }
-
-    path_env = getenv("PATH");
-    if (!path_env || path_env[0] == '\0')
-        return NULL;
-
-    path_dup = strdup(path_env);
-    if (!path_dup)
-    {
-        fprintf(stderr, "Allocation error\n");
-        exit(EXIT_FAILURE);
-    }
-
-    dir = strtok(path_dup, path_sep);
-    while (dir != NULL)
-    {
-        len = strlen(dir) + strlen(cmd) + 2;
-        fullpath = malloc(len);
-        if (!fullpath)
-        {
-            fprintf(stderr, "Allocation error\n");
-            exit(EXIT_FAILURE);
-        }
-        snprintf(fullpath, len, "%s/%s", dir, cmd);
-        if (access(fullpath, X_OK) == 0)
-        {
-            free(path_dup);
-            return fullpath;
-        }
-        free(fullpath);
-        dir = strtok(NULL, path_sep);
-    }
-
-    free(path_dup);
-    return NULL;
+	args[0] = NULL;
+	while (command != NULL && i < MAX_LEN - 1)
+	{
+		args[i] = command;
+		i++;
+		command = strtok(NULL, " \t");
+	}
+	args[i] = NULL;
 }
-
 /**
- * execute - execute command
- * @args: arguments array
- */
-void execute(char **args)
+ * process_commands - commands processor func
+ * @commands: commands
+ * @commands_array: array for all commands
+*/
+void process_commands(char *commands, char **commands_array)
 {
-    pid_t pid;
-    int status;
-    char *cmd_path;
+	char *command;
+	int a = 0;
 
-    if (args[0] == NULL)
-        return;
-
-    cmd_path = command_exists(args[0]);
-    if (!cmd_path)
-    {
-        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-        return;
-    }
-
-    pid = fork();
-    if (pid == 0)
-    {
-        execve(cmd_path, args, environ);
-        fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-        exit(127);
-    }
-    else if (pid > 0)
-    {
-        wait(&status);
-        (void)status;
-    }
-    else
-    {
-        perror("fork");
-    }
+	command = strtok(commands, "\n");
+	while (command != NULL)
+	{
+		commands_array[a] = command;
+		command = strtok(NULL, "\n");
+		a++;
+	}
+	commands_array[a] = NULL;
 }
+/**
+ * handle_commands_array - func for handling array of commands
+ * @commands_array: array of commands
+ */
+void handle_commands_array(char **commands_array)
+{
+	int a = 0;
+	char *command;
 
+	if (strcmp(commands_array[a], "exit") == 0)
+		exit(0);
+	else if (strcmp(commands_array[a], "env") == 0)
+		print_env();
+	else
+		while (commands_array[a] != NULL)
+		{
+			command = commands_array[a];
+			if (strcmp(command, "exit") == 0 && a > 0)
+				exit(2);
+			handle_command(command);
+			a++;
+		}
+}
+/**
+ * main - main func to process all functions
+ * Return: integer
+ */
 int main(void)
 {
-    char *line = NULL;
-    size_t len = 0;
-    char **args;
-    ssize_t read;
+	char commands[MAX_LEN];
+	char *commands_array[MAX_LEN];
+	ssize_t read_size;
 
-    while (1)
-    {
-        printf(":) ");
-        fflush(stdout);
+	while (1)
+	{
+		read_size = read(STDIN_FILENO, commands, MAX_LEN);
+		if (read_size == -1)
+		{
+			perror("Error reading command");
+			exit(EXIT_FAILURE);
+		}
+		else if (read_size == 0)
+			break;
+		commands[read_size] = '\0';
 
-        read = getline(&line, &len, stdin);
-        if (read == -1)
-        {
-            free(line);
-            break;
-        }
-
-        args = split_line(line);
-        execute(args);
-        free(args);
-    }
-
-    return 0;
+		process_commands(commands, commands_array);
+		handle_commands_array(commands_array);
+	}
+	return (0);
 }
