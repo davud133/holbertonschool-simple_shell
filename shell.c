6045a19243243817a71729287d9ced4fc6e67665
davud <davudgeroqiq@gmail.com>
2026-02-22 20:48:46 +0300

    new commit

--- shell.c
+++ shell.c
@@ -30,10 +30,8 @@ int main(void)
     char *line = NULL;
     size_t len = 0;
     ssize_t read_len;
-    pid_t pid;
     int interactive;
-    char *argv[2];
-    argv[1] = NULL;
+    pid_t pid;
 
     while (1)
     {
@@ -57,6 +55,16 @@ int main(void)
         if (line[0] == '\0')
             continue;
 
+        char *argv[64];
+        int argc = 0;
+        char *token = strtok(line, " ");
+        while (token != NULL && argc < 63)
+        {
+            argv[argc++] = token;
+            token = strtok(NULL, " ");
+        }
+        argv[argc] = NULL;
+
         pid = fork();
         if (pid < 0)
         {
@@ -66,13 +74,11 @@ int main(void)
         }
         else if (pid == 0)
         {
-            argv[0] = line;
-            if (execve(line, argv, environ) == -1)
+            if (execve(argv[0], argv, environ) == -1)
             {
                 write(2, "./simple_shell: No such file or directory\n", 43);
                 exit(1);
             }
-            exit(0);
         }
         else
             wait(NULL);
